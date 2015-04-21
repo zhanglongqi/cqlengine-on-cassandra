5081614ab9f1f8c2d4c1accfaa6fe7b20303f7c6
lqzhang <lqzhang@ntu.edu.sg>
2015-04-21 16:12:32 +0800

    adding example, can not work for now.

--- main.py
+++ main.py
@@ -9,8 +9,49 @@ from cqlengine import columns
 from cqlengine import Model
 
 
-class ExampleModel(Model):
-    example_id = columns.UUID(primary_key=True, default=uuid.uuid4)
-    example_type = columns.Integer(index=True)
-    created_at = columns.DateTime()
-    description = columns.Text(required=False)
+class StaffModel(Model):
+    name = columns.Text(primary_key=True, default='Unknown')
+    age = columns.Integer(index=True)
+    hp = columns.Text(required=False)
+
+# next, setup the connection to your cassandra server(s)...
+from cqlengine import connection
+
+# see http://datastax.github.io/python-driver/api/cassandra/cluster.html for options
+# the list of hosts will be passed to create a Cluster() instance
+connection.setup(['172.21.77.197'], "test", protocol_version=2)
+
+# ...and create your CQL table
+from cqlengine.management import sync_table
+
+sync_table(StaffModel)
+
+# now we can create some rows:
+em1 = StaffModel.create(name='a', age=1, hp='hp1')
+em2 = StaffModel.create(name='b', age=2, hp='hp1')
+em3 = StaffModel.create(name='c', age=3, hp='hp1')
+em4 = StaffModel.create(name='E', age=99, hp='hp1')
+em5 = StaffModel.create(name='F', age=100, hp='hp1')
+
+# and now we can run some queries against our table
+StaffModel.objects.count()
+
+q = StaffModel.objects(example_type=1)
+q.count()
+
+for instance in q:
+    print(instance.name)
+
+# here we are applying additional filtering to an existing query
+# query objects are immutable, so calling filter returns a new
+# query object
+'''
+>> > q2 = q.filter(example_id=em5.example_id)
+
+>> > q2.count()
+1
+>> > for instance in q2:
+    >> > print
+instance.description
+example5
+'''
